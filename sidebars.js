8b6fa7adf9bf7909800abcdf525cb0ec3b9f64ce
Radek Kyselý <radek.kysely@superface.ai>
2021-08-05 12:19:39 +0200

    doc: Adding Map guide (#8)
    * docs: Adding initial Map guide draft
    
    * chore: Fixing the link to unmerged page

--- sidebars.js
+++ sidebars.js
@@ -22,6 +22,7 @@ module.exports = {
         'guides/setup-the-environment',
         'guides/create-new-profile',
         'guides/add-new-provider',
+        'guides/map-capability-to-provider',
       ]
     },
   ],
