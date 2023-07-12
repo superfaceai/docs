0ea0d22bb2210df3f034d3bd681adaf4e2ef526a
Martyn Davies <martynrdavies@gmail.com>
2023-07-12 17:50:28 +0200

    add Lob example

--- sidebars.js
+++ sidebars.js
@@ -96,6 +96,7 @@ module.exports = {
       items: [
         'api-examples/hubspot',
         'api-examples/infobip',
+        'api-examples/lob',
         'api-examples/notion',
         'api-examples/pagerduty',
         'api-examples/pipedrive',
